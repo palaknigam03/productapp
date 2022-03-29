ac155d8c6a9c6c3d0b0658d464248c3024cccc5e
palaknigam03 <palaknigam03@gmail.com>
2022-03-29 16:02:46 +0530

    change

--- controller/admin.controller.js
+++ controller/admin.controller.js
@@ -4,14 +4,17 @@ const Admin = require('../model/admin.model');
 const {validationResult} =require('express-validator');
 exports.signup =(request,response)=>{
     const errors = validationResult(request);
-    if(!errors.isEmpty()){
-        return response.status(403).json({errors : errors.array()})
-    }
-  let userName = request.body.name;
+     if(!errors.isEmpty()){
+         //console.log(request.body);
+         return response.status(403).json({errors : errors.array()})
+     }
+         //console.log(request.body);
+
+  let username = request.body.username;
   let userEmail = request.body.email;
   let userPassword = request.body.password;
    
-  Admin.create({name:userName,email: userEmail,password: userPassword})
+  Admin.create({username:username,email: userEmail,password: userPassword})
   .then(result=>{
       console.log(result);
       return response.status(201).json(result);
--- model/admin.model.js
+++ model/admin.model.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const AdminSchema = new mongoose.Schema({
     username:{
         type:String,
+        required:true
     },
     email:{
         type :String,
