724efa67c9e928214f42f7dd354e93a4df091493
palaknigam03 <palaknigam03@gmail.com>
2022-03-30 13:53:41 +0530

    chang42

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect('mongodb+srv://root:root@cluster0.yozhs.mongodb.net/product?retryWrites=true&w=majority');
 const path = require('path');
 const bodyparser = require('body-parser');
+const cors = require('cors');
 
 const port = process.env.PORT || 3000
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
+app.use(cors());
 
 app.use("/api/admin",AdminRouter);
 
